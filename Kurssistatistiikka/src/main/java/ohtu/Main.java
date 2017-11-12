d74e1e227ee8e96c68e3054024dbacb6839a3c9c
techstalgist <mikko.i.ruuskanen@gmail.com>
2017-11-12 13:57:26 +0200

    t1

--- Kurssistatistiikka/src/main/java/ohtu/Main.java
+++ Kurssistatistiikka/src/main/java/ohtu/Main.java
@@ -1,7 +1,34 @@
 package ohtu;
 
+import com.google.gson.Gson;
+import java.io.IOException;
+import org.apache.http.client.fluent.Request;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // vaihda oma opiskelijanumerosi seuraavaan, ÄLÄ kuitenkaan laita githubiin omaa opiskelijanumeroasi
+        String studentNr = "";
+        if ( args.length>0) {
+            studentNr = args[0];
+        }
+
+        String url = "https://studies.cs.helsinki.fi/ohtustats/students/"+studentNr+"/submissions";
+
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+        
+        System.out.println("opiskelijanumero: " + studentNr + "\n");
+        int totalHours = 0;
+        int exercisesCount = 0;
+        for (Submission submission : subs) {
+            System.out.println(submission);
+            totalHours += submission.getHours();
+            exercisesCount += submission.getExercises().length;
+        }
+        System.out.println("\nyhteensä: " + exercisesCount + " tehtävää, " + totalHours + " tuntia.");
+
     }
 }
--- Kurssistatistiikka/src/main/java/ohtu/Submission.java
+++ Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -1,7 +1,27 @@
 package ohtu;
 
+import java.util.Arrays;
+
 public class Submission {
     private int week;
+    private int hours;
+    private int[] exercises;
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int[] getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(int[] exercises) {
+        this.exercises = exercises;
+    }
 
     public void setWeek(int week) {
         this.week = week;
@@ -10,10 +30,15 @@ public class Submission {
     public int getWeek() {
         return week;
     }
+    
+    private String exercisesToString() {
+        String exercisesAsString = Arrays.toString(exercises);
+        return exercisesAsString.substring(1, exercisesAsString.length()-1);
+    }
 
     @Override
     public String toString() {
-        return ""+week;
+        return " viikko " + week + ": tehtyjä tehtäviä yhteensä: " + exercises.length + ", aikaa kului " + hours + " tuntia, tehdyt tehtävät: " + exercisesToString();
     }
     
 }
